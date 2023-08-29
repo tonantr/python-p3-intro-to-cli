fa8abb642aca4a8234a50f3e8fdc7c8d418da51a
tonan <tonantruong@hotmail.com>
2023-08-29 11:02:37 -0400

    Intro to CLIs

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
+def create_grade_report(student_grades):
+    with open('reports/grade_report.txt', 'w') as gr:
+        for grade in student_grades:
+            gr.write(grade + '\n')
+
 if __name__ == '__main__':
-    pass
+    student_grades = []
+    grade = input('Student name, grade: ')
+    while grade:
+        student_grades.append(grade)
+        grade = input('Student name, grade: ')
+    create_grade_report(student_grades)
+
+
+# class MyClass:
+#     def __init__(self, user_input)
+#         self.value = user_input
+
+# def my_function(my_object):
+#     # returns a final value for the CLI workflow
+
+# if __name__ == '__main__':
+#     user_input = input("Enter something here: ")
+#     my_object = MyClass(user_input)
+#     print(my_function(my_object))
